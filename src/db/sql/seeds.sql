89e937deb0f3bbda5a20bc1acb2bcb37573cca02
Robert Pearce <me@robertwpearce.com>
2016-05-02 09:12:22 -0400

    Prettified seeds

--- src/db/sql/seeds.sql
+++ src/db/sql/seeds.sql
@@ -3,16 +3,16 @@
 ------------------------------------------------------------
 
 INSERT into places (name, slug, image) VALUES
-  ('DeKalb Market', 'dekalb-market-brooklyn', NULL),
-  ('BoxPark', 'boxpark-london', NULL),
-  ('ReStart Container Mall', 'restart-container-mall-christchurch', NULL),
-  ('Market 707', 'market-707-toronto', NULL),
-  ('Copenhagen Street Food', 'copenhagen-street-food-copenhagen', NULL),
-  ('Pop Brixton', 'pop-brixton-london', NULL),
-  ('Downtown Container Park', 'downtown-container-park-las-vegas', NULL),
-  ('Wapping Wharf', 'whapping-wharf-bristol', NULL),
-  ('Eat Street Markets', 'eat-street-markets-brisbane', NULL),
-  ('Nightquarter', 'nightquarter-queensland', NULL),
-  ('Granary Row', 'granary-row-salt-lake-city', NULL),
-  ('Container Market', 'container-market-thailand', NULL)
+  ('DeKalb Market'          , 'dekalb-market-brooklyn'             , NULL),
+  ('BoxPark'                , 'boxpark-london'                     , NULL),
+  ('ReStart Container Mall' , 'restart-container-mall-christchurch', NULL),
+  ('Market 707'             , 'market-707-toronto'                 , NULL),
+  ('Copenhagen Street Food' , 'copenhagen-street-food-copenhagen'  , NULL),
+  ('Pop Brixton'            , 'pop-brixton-london'                 , NULL),
+  ('Downtown Container Park', 'downtown-container-park-las-vegas'  , NULL),
+  ('Wapping Wharf'          , 'whapping-wharf-bristol'             , NULL),
+  ('Eat Street Markets'     , 'eat-street-markets-brisbane'        , NULL),
+  ('Nightquarter'           , 'nightquarter-queensland'            , NULL),
+  ('Granary Row'            , 'granary-row-salt-lake-city'         , NULL),
+  ('Container Market'       , 'container-market-thailand'          , NULL)
 ;
