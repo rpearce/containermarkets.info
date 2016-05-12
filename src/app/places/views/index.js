d7c00bc84f59177ff9e0d77a13f76b02889fc105
Robert Pearce <me@robertwpearce.com>
2016-05-12 05:08:11 -0400

    Put up default index page linking to places and set up nodemon

--- src/app/places/views/index.js
+++ src/app/places/views/index.js
@@ -3,7 +3,27 @@
 const layout = require('../../../layouts')
 
 module.exports = (places) => {
-  const title = 'Container Markes aggregator'
-  const body = 'CM homepage'
+  const title = 'Container Markets Information'
+  const body = buildBody(places)
   return layout({ title, body })
+}
+
+const buildBody = (places) =>
+  `<header>
+    <h1>Container Markets Information</h1>
+  </header>
+  <section>
+    ${buildPlaces(places)}
+  </section>`
+
+const buildPlaces = (places) => {
+  if (places.length > 0) {
+    return `<ul>${places.map(buildPlace).join('')}</ul>`
+  }
+}
+
+const buildPlace = (place) => {
+  return `<li>
+    <a href="/${place.slug}">${place.name}</a>
+  </li>`
 }
