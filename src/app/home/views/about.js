3261420b58126c9e9133a49f10a2e66d13f7082c
Robert Pearce <me@robertwpearce.com>
2016-05-12 04:52:29 -0400

    Couple of page header updates for the future

--- src/app/home/views/about.js
+++ src/app/home/views/about.js
@@ -4,6 +4,14 @@ const layout = require('../../../layouts')
 
 module.exports = () => {
   const title = 'About'
-  const body = 'This is the about page'
+  const body = buildBody()
   return layout({ title, body })
 }
+
+const buildBody = () =>
+  `<header>
+    <h1>What is this?</h1>
+  </header>
+  <section>
+    <p>My partner and I enjoy visiting container malls/markets in our travels and thought this would be a useful tool for mapping and finding more!</p>
+  </section>`
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,26 @@
 'use strict'
 
-module.exports = ({ title, body }) =>
+//<meta property="og:image" content="${imageURI}">
+//<link href="http://fonts.googleapis.com/css?family=Lato:400" rel="stylesheet" type="text/css">
+
+module.exports = ({ title, body, description, type = 'website', uri = 'http://containermarkets.info' }) =>
   `<html lang="en">
     <head>
       <title>${title}</title>
+
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1">
+      <meta name="google-site-verification" content="QX4bn65yOXBfgnwxlO09yIBc1H8blur-Erx3lmsDVhU">
+      <meta name="description" content="${description}">
+
+      <meta property="og:site_name" content="containermarkets.info">
+      <meta property="og:title" content="${title}">
+      <meta property="og:url" content="${uri}">
+      <meta property="og:description" content="${description}">
+      <meta property="og:type" content="${type}">
+
+      <link rel="canonical" href="${uri}">
       <link href="/app.css" rel="stylesheet">
     </head>
     <body>${body}</body>
