32a0a99460d8b30374311af0afe8d428c9d869d7
Robert Pearce <me@robertwpearce.com>
2016-05-19 18:36:45 +0100

    added mailchimp newsletter form to app and added note to signing up

--- src/app/places/views/index.js
+++ src/app/places/views/index.js
@@ -27,12 +27,33 @@ const buildBody = (places) =>
       <h3>Container Markets</h3>
       ${buildPlaces(places)}
     </div>
+  </section>
+  <section class="signup section">
+    <div class="l--constrained">
+      <h3>Get info on new container markets</h3>
+      <p>Note: we will <em>never</em> give out your email nor spam you. We just want people to enjoy container markets as much as we do.</p>
+      <!-- Begin MailChimp Signup Form -->
+      <form action="//containermarkets.us13.list-manage.com/subscribe/post?u=2df44e8960266388bff165fa6&amp;id=cb3f0eab65" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+        <div>
+          <label for="mce-EMAIL">Email Address</label>
+          <input type="email" value="" name="EMAIL" id="mce-EMAIL" placeholder="you@greatemail.com">
+        </div>
+        <div id="mce-responses" class="clear">
+          <div class="response" id="mce-error-response" style="display:none"></div>
+          <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_2df44e8960266388bff165fa6_cb3f0eab65" tabindex="-1" value=""></div>
+        <div class="clear"><input type="submit" value="Subscribe" name="subscribe"></div>
+      </form>
+    </div>
+    <!--End mc_embed_signup-->
   </section>`
 
 const buildPlaces = (places) => {
   if (places.length > 0) {
     return `<ul class="places__list">${places.map(buildPlace).join('')}</ul>`
   }
+  return '';
 }
 
 const buildPlace = (place) => {
