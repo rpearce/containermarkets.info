964e59a58d6074114a215ca0d3191ddb8be912b4
Robert Pearce <me@robertwpearce.com>
2016-05-25 16:56:50 +0100

    Make sure each input is required

--- src/app/places/views/_form.js
+++ src/app/places/views/_form.js
@@ -10,31 +10,31 @@ module.exports = ({
     ${buildErrors(errors)}
     <div class="form__section">
       <label for="place_name">Name</label>
-      <input type="text" name="place[name]" id="place_name" value="${place.name}"  />
+      <input type="text" name="place[name]" id="place_name" value="${place.name}" required />
     </div>
     <div class="form__section">
       <label for="place_slug">Slug</label>
-      <input type="text" name="place[slug]" id="place_slug" value="${place.slug}"  />
+      <input type="text" name="place[slug]" id="place_slug" value="${place.slug}" required />
     </div>
     <div class="form__section">
       <label for="place_address">Address</label>
-      <input type="text" name="place[address]" id="place_address" value="${place.address}"  />
+      <input type="text" name="place[address]" id="place_address" value="${place.address}" required />
     </div>
     <div class="form__section">
       <label for="place_lat">Latitude</label>
-      <input type="text" name="place[lat]" id="place_lat" value="${place.lat}"  />
+      <input type="text" name="place[lat]" id="place_lat" value="${place.lat}" required />
     </div>
     <div class="form__section">
       <label for="place_long">Longitude</label>
-      <input type="text" name="place[long]" id="place_long" value="${place.long}"  />
+      <input type="text" name="place[long]" id="place_long" value="${place.long}" required />
     </div>
     <div class="form__section">
       <label for="place_description">Description</label>
-      <input type="text" name="place[description]" id="place_description" value="${place.description}"  />
+      <input type="text" name="place[description]" id="place_description" value="${place.description}" required />
     </div>
     <div class="form__section">
       <label for="place_content">Content</label>
-      <textarea name="place[content]" id="place_content" placeholder="Enter markdown content here..." rows="1" data-js="textarea" >${place.content}</textarea>
+      <textarea name="place[content]" id="place_content" placeholder="Enter markdown content here..." rows="1" data-js="textarea" required>${place.content}</textarea>
     </div>
     <div class="form__section">
       <button type="submit">Submit</button>
